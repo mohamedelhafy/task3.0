516a3f17cce66c62079502daa5a115b710dcf3d8
mohamed elhafy <mohamedelhafy2017@gmail.com>
2019-02-15 19:34:06 +0200

    mohamed elhafy

---
+++ Html/error.php
@@ -0,0 +1,17 @@
+<?php
+
+  $_SESSION["message"]= $_POST['message'];
+
+
+  if (isset($_SESSION["message"]) && !empty($_SESSION["message"])) {
+      $msg = $_SESSION["message"];
+      echo "<div class='msgbox'>" . $msg . "</div>";
+      unset($_SESSION['message']);
+
+      echo '<input type="submit" value="sign up" class="btn" name="sign_up">';
+    }
+
+
+
+
+ ?>
