80855928e501d04180873f63d9005ee022922640
mohamed elhafy <mohamedelhafy2017@gmail.com>
2019-02-15 18:17:59 +0200

    first commit

---
+++ php/sign in3.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>sign in</title>
+        <link rel="stylesheet" href="../css/sign%20in.css" >
+    </head>
+    <body>
+        <div class="signin">
+            <form action="profile.php" method="post">
+                <h2>login</h2>
+
+                <p>Email</p>
+                <input type="email" placeholder="Enter your email" name="email">
+                <p>Password</p>
+                <input type="password" placeholder="Enter your password" name="password">
+               <input type="submit" value="sign in" class="btn" name="sign_in">
+                <a href="#">Forget password</a>
+            </form>
+        <div>
+
+    </body>
+</html>
